b4f5f98717534fe443e62253bd83d2800400b9a4
Bruno Farias <contato.brunofarias@gmail.com>
2021-01-04 22:20:26 -0300

    First Commit.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # sprint-rest-api
 Exemplo de uma API Rest com Java.
+
+
+
+- Spring Initializer.
+- Dependências
+	- Web.
+	- JPA CRUD.
+- Bean Validation
+- Swagger
+- Qual banco de dados.
+- Properties
+
+docker run --name postgresql-container -p 5432:5432 -e POSTGRES_PASSWORD=myPassword -d postgres
+Uso do postman
+
---
+++ src/main/java/br/com/brunofarias/apirest/controller/ClienteDto.java
@@ -0,0 +1,52 @@
+package br.com.brunofarias.apirest.controller;
+
+import java.time.LocalDate;
+
+public class ClienteDto {
+    private String nome;
+    private String email;
+    private String cpf;
+    private LocalDate dataNascimento;
+
+    public ClienteDto() {
+    }
+
+    public ClienteDto(String nome, String email, String cpf, LocalDate dataNascimento) {
+        this.nome = nome;
+        this.email = email;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
